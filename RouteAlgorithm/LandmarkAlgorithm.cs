07c7c366cf46a634f286d87aa29eb41e2f59775d
Alilna Wang <wangpengxia2015@outlook.com>
2016-01-21 18:15:12 +0800

    implement landmark algorithm

---
+++ RouteAlgorithm/LandmarkAlgorithm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RouteAlgorithm
+{
+    public class LandmarkAlgorithm
+    {
+        private RoadNetwork graph;
+        private DijkstraAlgorithm dijkstra;
+        private List<double> landmarks;
+        int numOfLandmarks;
+
+        public LandmarkAlgorithm() { }
+
+        public LandmarkAlgorithm(RoadNetwork graph)
+        {
+            this.graph = graph;
+        }
+
+        public void SelectLandmarks(int numOfLandmarks)
+        {
+            if (numOfLandmarks < graph.Nodes.Count())
+            {
+                this.numOfLandmarks = numOfLandmarks;
+                for (int i=0;i<numOfLandmarks;i++)
+                {
+
+                }
+            }
+        }
+
+        public void ComputLandmarks()
+        {
+
+        }
+
+        
+
+        public void ComputShortPath()
+        { }
+    }
+}
